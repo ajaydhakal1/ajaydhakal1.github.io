0d203779b14cc886004ec60c02b5f21071a02953
ajaydhakal01 <ajaydhakal110@gmail.com>
2023-11-06 14:44:39 +0545

    portfolio

---
+++ src/Contact.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import styled from "styled-components";
+
+const Contact = () => {
+  const Wrapper = styled.section`
+    padding: 9rem 0 5rem 0;
+
+    .container {
+      margin-top: 6rem;
+      text-align: center;
+
+      .contact-form {
+        max-width: 50rem;
+        margin: auto;
+
+        .contact-inputs {
+          display: flex;
+          flex-direction: column;
+          gap: 3rem;
+
+          input[type="submit"] {
+            cursor: pointer;
+            transition: all 0.2s;
+
+            &:hover {
+              background-color: ${({ theme }) => theme.colors.white};
+              border: 1px solid ${({ theme }) => theme.colors.btn};
+              color: ${({ theme }) => theme.colors.btn};
+              transform: scale(0.9);
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  return (
+    <Wrapper>
+      <h2 className="common-heading">Feel Free to Contact us</h2>
+
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28148.16943739452!2d83.7740993!3d28.130618149999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3995c2c63e6a6e05%3A0x3807e631b0b33884!2sPanchamul%2033800!5e0!3m2!1sen!2snp!4v1699257703085!5m2!1sen!2snp%22%20style=%22border:0;%22%20allowfullscreen=%22%22%20loading=%22lazy%22%20referrerpolicy=%22no-referrer-when-downgrade"
+        width="100%"
+        height="450"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"></iframe>
+
+      <div className="container">
+        <div className="contact-form">
+          <form
+            action="https://formspree.io/f/xgedgjkr"
+            method="POST"
+            className="contact-inputs">
+            <input
+              type="text"
+              name="username"
+              placeholder="username"
+              autoComplete="off"
+              required
+            />
+
+            <input
+              type="email"
+              name="Email"
+              placeholder="Email"
+              autoComplete="off"
+              required
+            />
+
+            <textarea
+              name="message"
+              cols="30"
+              rows="6"
+              autoComplete="off"
+              required></textarea>
+
+            <input type="submit" value="send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Contact;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,153 @@
+import React from "react";
+import styled from "styled-components";
+import { Button } from "../styles/Button";
+import { NavLink } from "react-router-dom";
+import { FaFacebook, FaInstagram, FaTiktok } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <Wrapper>
+      {/* footer section  */}
+
+      <footer>
+        <div className="container grid grid-four-column">
+          <div className="footer-about">
+            <h3>AJAY DHAKAL</h3>
+            <p>A frontend developer & Digital Entrepreneur.</p>
+          </div>
+
+          {/* 2nd column */}
+          <div className="footer-subscribe">
+            <h3>Subscribe to get important updates</h3>
+            <form action="#">
+              <input
+                type="email"
+                required
+                autoComplete="off"
+                placeholder="Email"
+              />
+              <input type="submit" value="Subscribe" />
+            </form>
+          </div>
+
+          {/* 3rs column  */}
+          <div className="footer-social">
+            <h3>Follows Us</h3>
+            <div className="footer-social--icons">
+              <div>
+                <a target="_main" href="https://www.facebook.com/profile.php?id=100093652912094">
+                <FaFacebook className="icons" />
+                </a>
+              </div>
+              <div>
+                <a href="https://www.instagram.com/ajay_dha.kal/" target="_main">
+                <FaInstagram className="icons" />
+                </a>
+              </div>
+              <div>
+                <a
+                  href="https://www.tiktok.com/@ajay.dhakal"
+                  target="_blank">
+                  <FaTiktok className="icons" />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* 4th column  */}
+          <div className="footer-contact">
+            <h3>Whats App</h3>
+            <h3>+977 9748217212</h3>
+          </div>
+        </div>
+
+        {/* bottom section  */}
+        <div className="footer-bottom--section">
+          <hr />
+          <div className="container grid grid-two-column">
+            <p>
+              @{new Date().getFullYear()} Ajay Dhakal. All Rights Reserved
+            </p>
+          </div>
+        </div>
+      </footer>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.section`
+  .contact-short {
+    max-width: 60vw;
+    margin: auto;
+    padding: 5rem 10rem;
+    background-color: ${({ theme }) => theme.colors.bg};
+    border-radius: 1rem;
+    box-shadow: ${({ theme }) => theme.colors.shadowSupport};
+    transform: translateY(50%);
+  }
+  .contact-short-btn {
+    justify-self: end;
+    align-self: center;
+  }
+
+  footer {
+    padding: 14rem 0 9rem 0;
+    background-color: ${({ theme }) => theme.colors.footer_bg};
+
+    h3 {
+      color: ${({ theme }) => theme.colors.hr};
+      margin-bottom: 2.4rem;
+    }
+    p {
+      color: ${({ theme }) => theme.colors.white};
+    }
+    .footer-social--icons {
+      display: flex;
+      gap: 2rem;
+
+      div {
+        padding: 1rem;
+        border-radius: 50%;
+        border: 2px solid ${({ theme }) => theme.colors.white};
+
+        .icons {
+          color: ${({ theme }) => theme.colors.white};
+          font-size: 2.4rem;
+          position: relative;
+          cursor: pointer;
+        }
+      }
+    }
+
+    .footer-bottom--section {
+      padding-top: 9rem;
+
+      hr {
+        margin-bottom: 2rem;
+        color: ${({ theme }) => theme.colors.hr};
+        height: 0.1px;
+      }
+    }
+  }
+
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+    .contact-short {
+      max-width: 95vw;
+      padding: 2rem 0rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .contact-short-btn {
+        text-align: center;
+        justify-self: flex-start;
+      }
+    }
+
+    footer .footer-bottom--section {
+      padding-top: 3.2rem;
+    }
+  }
+`;
+
+export default Footer;
